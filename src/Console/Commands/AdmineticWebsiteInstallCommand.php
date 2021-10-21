ce248bbb014e3c5d20f41f9089e6aca8f7c85cbd
pratiksh404 <pratikdai404@gmail.com>
2021-10-21 15:39:23 +0545

    install command improvements

--- src/Console/Commands/AdmineticWebsiteInstallCommand.php
+++ src/Console/Commands/AdmineticWebsiteInstallCommand.php
@@ -41,10 +41,17 @@ class AdmineticWebsiteInstallCommand extends Command
         if ($this->confirm('Do you wish to seed module permission?')) {
             Artisan::call('adminetic:website-permission');
         }
+        $this->info('Adminetic website permission seeded ... ✅');
+        Artisan::call('vendor:publish', [
+            '--tag' => ['website-config']
+        ]);
         Artisan::call('vendor:publish', ['--provider' => 'CyrildeWit\EloquentViewable\EloquentViewableServiceProvider', '--tag' => 'migrations']);
         Artisan::call('vendor:publish', ['--provider' => 'Spatie\Analytics\AnalyticsServiceProvider']);
+        $this->info('Adminetic website config file published ... ✅');
         Artisan::call('install:adminetic-category');
+        $this->info('Adminetic category installed ... ✅');
         Artisan::call('migrate');
+        $this->info('Adminetic website module migration complete ... ✅');
         $this->info('Adminetic Website Installed.');
         $this->info('Star to the admenictic repo would be appreciated.');
     }
